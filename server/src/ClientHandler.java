0aaf1eb1c5cbac234c123b8e0510a66b5c4ee902
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2023-01-16 14:40:40 -0500

    WIP servers resends messages to other clients

--- server/src/ClientHandler.java
+++ server/src/ClientHandler.java
@@ -2,33 +2,53 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ClientHandler extends Thread { // pour traiter la demande de chaque client sur un socket particulier
     private Socket socket;
     private int clientNumber;
 
-    public ClientHandler(Socket socket, int clientNumber) {
+    private ConcurrentLinkedQueue<String> threadMessageQueue;
+
+    private ConcurrentLinkedQueue<String> sharedMessageQueue;
+
+    public ClientHandler(Socket socket, int clientNumber, ConcurrentLinkedQueue<String> messageQueue) {
         this.socket = socket;
         this.clientNumber = clientNumber;
+        this.sharedMessageQueue = messageQueue;
+        this.threadMessageQueue = new ConcurrentLinkedQueue<>();
         System.out.println("New connection with client#" + clientNumber + " at" + socket);
+    }
+
+    public int getClientNumber() {
+        return clientNumber;
+    }
+
+    public void addMessage(String message) {
+        threadMessageQueue.offer(message);
     }
 
     public void run() { // Création de thread qui envoi un message à un client
         try (
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream()); // création de canal d’envoi
                 DataInputStream in = new DataInputStream(socket.getInputStream());
-                ) {
+        ) {
             out.writeUTF("Hello from server - you are client#" + clientNumber); // envoi de message
 
             while (true) {
                 if (in.available() > 0) {
                     String message = in.readUTF();
 
-                    if (message.equals("EXIT")) {
+                    message = "Client #" + clientNumber + " : " + message;
+                    sharedMessageQueue.offer(message);
+                    if (message.contains("EXIT")) {
                         break;
-                    } else {
-                        System.out.println("Client #" + clientNumber + " : " + message);
                     }
+                }
+
+                while (!threadMessageQueue.isEmpty()) {
+                    String outboundMessage = threadMessageQueue.poll();
+                    out.writeUTF(outboundMessage);
                 }
             }
         } catch (IOException e) {
--- server/src/Main.java
+++ server/src/Main.java
@@ -1,11 +1,24 @@
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Main {
     private static ServerSocket Listener; // Application Serveur
 
+    private static ConcurrentLinkedQueue<String> sharedMessagesQueue;
+
+    private static HashMap<Integer, ClientHandler> clients;
+
+    private static boolean clientDisconnected(int clientNumber, String message) {
+        return message.contains("EXIT") && new Scanner(message).useDelimiter("\\D+").nextInt() == clientNumber;
+    }
+
     public static void main(String[] args) throws Exception {
+        sharedMessagesQueue = new ConcurrentLinkedQueue<>();
+        clients = new HashMap();
         // Compteur incrémenté à chaque connexion d'un client au serveur
         int clientNumber = 0;
         // Adresse et port du serveur
@@ -23,7 +36,22 @@ public class Main {
             while (true) {
                 // Important : la fonction accept() est bloquante: attend qu'un prochain client se connecte
                 // Une nouvetle connection : on incémente le compteur clientNumber
-                new ClientHandler(Listener.accept(), clientNumber++).start();
+                ClientHandler client = new ClientHandler(Listener.accept(), clientNumber++, sharedMessagesQueue);
+                clients.put(clientNumber - 1, client);
+                client.start();
+
+                while (!sharedMessagesQueue.isEmpty()) {
+                    String message = sharedMessagesQueue.poll();
+                    System.out.println(message);
+
+                    for (ClientHandler ch : clients.values()) {
+                        ch.addMessage(message);
+
+                        if (clientDisconnected(client.getClientNumber(), message)) {
+                            clients.remove(client.getClientNumber());
+                        }
+                    }
+                }
             }
         } finally {
             // Fermeture de la connexion
