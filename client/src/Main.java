244b8127c13802b0b16b3fbf31bd80c099d2c61a
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2023-01-16 19:24:37 -0500

    Moved logic to receive messages on the client on another thread

--- client/src/Main.java
+++ client/src/Main.java
@@ -13,24 +13,18 @@ public class Main {
         try (
                 // Création d'une nouvelle connexion aves le serveur
                 Socket socket = new Socket(serverAddress, port);
-                // Céatien d'un canal entrant pour recevoir les messages envoyés, par le serveur
-                DataInputStream in = new DataInputStream(socket.getInputStream());
                 // Canal sortant pour envoyer des messages au serveur
                 DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
         ) {
             System.out.println("Serveur lancé sur [" + serverAddress + ":" + port + "]");
 
-            out.writeUTF("Hello from Client");
+            MessageReceiver messageReceiver = new MessageReceiver(socket.getInputStream());
+
+            messageReceiver.start();
 
             String userMessage;
             while (!Objects.equals(userMessage = stdIn.readLine(), "EXIT")) {
-                if (in.available() > 0) {
-                    // Attente de la réception d'un message envoyé par le, server sur le canal
-                    String message = in.readUTF();
-                    System.out.println(message);
-                }
-
                 if (userMessage.length() > 0) {
                     out.writeUTF(userMessage);
                 }
