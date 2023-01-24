f5ad27018dd9abcd6f42eb7cb70d6a11b4aecb97
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2023-01-23 19:46:36 -0500

    Extracted client logic to class

---
+++ client/src/Client.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Objects;
+
+public class Client {
+    public void start() {
+        InputsHandler input = new InputsHandler();
+        String serverAddress = "";
+        int port = 0;
+
+        try {
+            serverAddress = input.checkIp();
+            port = input.checkPort();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        try (
+                // Création d'une nouvelle connexion aves le serveur
+                Socket socket = new Socket(serverAddress, port);
+                // Canal sortant pour envoyer des messages au serveur
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        ) {
+            System.out.println("Serveur lancé sur [" + serverAddress + ":" + port + "]");
+
+            MessageReceiver messageReceiver = new MessageReceiver(socket.getInputStream());
+
+            messageReceiver.start();
+
+            String userMessage;
+            while (!Objects.equals(userMessage = stdIn.readLine(), "EXIT")) {
+                if (userMessage.length() > 0) {
+                    out.writeUTF(userMessage);
+
+                }
+            }
+
+            out.writeUTF(userMessage);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
--- client/src/Main.java
+++ client/src/Main.java
@@ -1,41 +1,6 @@
-import java.io.*;
-import java.net.Socket;
-import java.util.Objects;
-
-
-// Application client
 public class Main {
-    private static Socket socket;
-
-    public static void main(String[] args) throws Exception {
-        // Adresse et port du serveur
-
-
-        InputsHandler  input = new InputsHandler();
-        String serverAddress = input.checkIp();
-        int port = input.checkPort();
-        try (
-                // Création d'une nouvelle connexion aves le serveur
-                Socket socket = new Socket(serverAddress, port);
-                // Canal sortant pour envoyer des messages au serveur
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-        ) {
-            System.out.println("Serveur lancé sur [" + serverAddress + ":" + port + "]");
-
-            MessageReceiver messageReceiver = new MessageReceiver(socket.getInputStream());
-
-            messageReceiver.start();
-
-            String userMessage;
-            while (!Objects.equals(userMessage = stdIn.readLine(), "EXIT")) {
-                if (userMessage.length() > 0) {
-                    out.writeUTF(userMessage);
-
-                }
-            }
-
-            out.writeUTF(userMessage);
-        }
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.start();
     }
 }
